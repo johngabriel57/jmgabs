7655a818a37e81186fa315eedd301f123601fd6c
johngabriel57 <57347050+johngabriel57@users.noreply.github.com>
2019-11-16 23:25:10 +0800

    Delete getGuessedWord.py

--- getGuessedWord.py
+++
@@ -1,9 +0,0 @@
-def getGuessedWord(secretWord, lettersGuessed):
-    
-    guess= ''
-    for char in secretWord:
-        if char in lettersGuessed:
-            guess= guess + char
-        else:
-            guess= guess + ' _ '
-    return guess
